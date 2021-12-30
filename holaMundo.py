7864b83beaf53b21f2ac4595411bebf45899839a
David Padilla <davidpaag2002@gmail.com>
2021-12-30 18:36:22 +0100

    Add primer fichero

---
+++ holaMundo.py
@@ -0,0 +1,13 @@
+print("holaaa world!!");
+
+#Comentario de una línea
+'''
+Comentario multilinea
+'''
+print("Fundamentos","Programación","en",sep="***",end="...")
+print("Python")
+
+#Uso de input y casteo
+algo = float(input("Inserta un número: "))
+resultado = algo ** 2.0
+print(algo, "al cuadrado es", resultado)
